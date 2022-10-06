de22554f112d573ec29771860a2717168d24d7a8
arinvolosh <arinko8150@gmail.com>
2022-10-06 11:42:58 +0300

    fix

--- ServiceTests/ExportTest.cs
+++ ServiceTests/ExportTest.cs
@@ -1,13 +1,14 @@
 ﻿using ExportTool;
 using Xunit;
 using Services;
+using Models;
 
 namespace ServiceTests
 {
     public class ExportTest
     {
         [Fact]
-        public void ExportClientTest()
+        public void WriteClientTest()
         {
             //Arrange
             string pathToDirectory = Path.Combine("C:\\Курс\\.net-course-2022.Voloshina", "ExportData");
@@ -19,6 +20,33 @@ namespace ServiceTests
             var tests = clientExporter.ReadClientFromCsv(pathToDirectory, fileName);
             //Assert
             Assert.NotNull(tests);
+        }
+        [Fact]
+        public void ReadClientTest()
+        {
+            //Arrange
+            var service = new ClientService();
+
+            var clients = new List<Client>();
+            var clientIvan = new Client();
+            
+            clients.Add(clientIvan);
+
+
+            //Act
+            string pathToDirectory = Path.Combine("C:\\Курс\\.net-course-2022.Voloshina", "ExportData");
+            string fileName = "client.csv";
+            ExportService clientExporter = new ExportService(pathToDirectory, fileName);
+            clientExporter.WriteClientToCsv(clients);
+
+            var clientsRead = clientExporter.ReadClientFromCsv();
+            foreach (var client in clientsRead)
+            {
+                service.AddClient(client);
+            }
+
+            //Assert
+            Assert.NotEmpty(clientsRead);
         }
 
     }
